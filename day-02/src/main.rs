c94604b62a3811e28f913840e480d83b3d6e341a
Jake Goulding <jake.goulding@gmail.com>
2025-12-02 13:02:42 -0500

    Day 02 - Part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,9 +1,17 @@
+#![feature(int_format_into)]
+
+use core::fmt::NumBuffer;
+
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
     let part1 = sum_of_invalid_ids(INPUT);
     assert_eq!(12586854255, part1);
     println!("{part1}");
+
+    let part2 = sum_of_all_invalid_ids(INPUT);
+    assert_eq!(17298174201, part2);
+    println!("{part2}");
 }
 
 fn sum_of_invalid_ids(s: &str) -> u64 {
@@ -30,6 +38,23 @@ fn sum_of_invalid_ids(s: &str) -> u64 {
         .sum()
 }
 
+fn sum_of_all_invalid_ids(s: &str) -> u64 {
+    id_ranges(s)
+        .flat_map(|(s, e)| {
+            (s..=e).flat_map(|n| {
+                let mut buf = NumBuffer::new();
+                let buf = n.format_into(&mut buf);
+                let buf = buf.as_bytes();
+
+                let mut sequence_lengths = 1..buf.len();
+                let any_sequence_repeated = sequence_lengths.any(|l| all_chunks_same(buf, l));
+
+                any_sequence_repeated.then_some(n)
+            })
+        })
+        .sum()
+}
+
 fn id_ranges(s: &str) -> impl Iterator<Item = (u64, u64)> {
     s.split(",").map(|p| {
         let (s, e) = p.trim().split_once("-").expect("pair malformed");
@@ -48,6 +73,20 @@ fn upper(i: u64) -> Option<u64> {
         let k = 10u64.pow(n_digits / 2);
         i / k
     })
+}
+
+fn all_chunks_same<T: Eq>(buf: &[T], n: usize) -> bool {
+    let mut chunks = buf.chunks_exact(n);
+
+    if !chunks.remainder().is_empty() {
+        return false;
+    }
+
+    let Some(head) = chunks.next() else {
+        return false;
+    };
+
+    chunks.all(|c| head == c)
 }
 
 #[cfg(test)]
@@ -75,5 +114,18 @@ mod test {
         assert_eq!(Some(1), upper(12));
         assert_eq!(None, upper(123));
         assert_eq!(Some(12), upper(1234));
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(4174379265, sum_of_all_invalid_ids(EXAMPLE));
+    }
+
+    #[test]
+    fn all_chunks_same_exercise() {
+        assert!(all_chunks_same(b"12341234", 4));
+        assert!(all_chunks_same(b"123123123", 3));
+        assert!(all_chunks_same(b"1212121212", 2));
+        assert!(all_chunks_same(b"1111111", 1));
     }
 }
