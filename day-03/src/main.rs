eeaa41df9a6e29df8c6ba76b50d1ffb6f453e039
Jake Goulding <jake.goulding@gmail.com>
2025-12-03 09:20:59 -0500

    Day 03 - Part 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,23 +1,25 @@
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
-    let part1 = sum_of_max_joltage(INPUT);
+    let part1 = sum_of_max_joltage::<2>(INPUT);
     // Was preferring the last maximum value instead of first when equal
     assert!(part1 > 16923);
     assert_eq!(17100, part1);
     println!("{part1}");
+
+    let part2 = sum_of_max_joltage::<12>(INPUT);
+    assert_eq!(170418192256861, part2);
+    println!("{part2}");
 }
 
-fn sum_of_max_joltage(s: &str) -> u32 {
+fn sum_of_max_joltage<const N_BATTERIES: usize>(s: &str) -> u64 {
     s.lines()
         .map(|l| l.chars().map(|c| c.to_digit(10).expect("invalid digit")))
-        .map(max_joltage)
+        .map(max_joltage::<N_BATTERIES>)
         .sum()
 }
 
-fn max_joltage(batteries: impl IntoIterator<Item = u32>) -> u32 {
-    const N_BATTERIES: usize = 2;
-
+fn max_joltage<const N_BATTERIES: usize>(batteries: impl IntoIterator<Item = u32>) -> u64 {
     let batteries = batteries.into_iter().collect::<Vec<_>>();
 
     let mut start_idx = 0;
@@ -33,11 +35,11 @@ fn max_joltage(batteries: impl IntoIterator<Item = u32>) -> u32 {
                 .max_by(|a, b| a.1.cmp(&b.1).then_with(|| b.0.cmp(&a.0)))
                 .expect("Could not find a maximum");
 
-            start_idx = max_idx + 1;
+            start_idx += max_idx + 1;
 
             max
         })
-        .fold(0, |sum, n| sum * 10 + n)
+        .fold(0, |sum, n| sum * 10 + u64::from(n))
 }
 
 #[cfg(test)]
@@ -48,13 +50,18 @@ mod test {
 
     #[test]
     fn part1_example() {
-        assert_eq!(357, sum_of_max_joltage(EXAMPLE));
+        assert_eq!(357, sum_of_max_joltage::<2>(EXAMPLE));
     }
 
     #[test]
     fn max_joltage_prefers_first_of_equal() {
-        assert_eq!(66, max_joltage([6, 6]));
-        assert_eq!(66, max_joltage([6, 1, 6]));
-        assert_eq!(66, max_joltage([6, 1, 6, 1]));
+        assert_eq!(66, max_joltage::<2>([6, 6]));
+        assert_eq!(66, max_joltage::<2>([6, 1, 6]));
+        assert_eq!(66, max_joltage::<2>([6, 1, 6, 1]));
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(3121910778619, sum_of_max_joltage::<12>(EXAMPLE));
     }
 }
