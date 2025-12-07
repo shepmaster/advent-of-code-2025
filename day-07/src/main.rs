2c11179b425ee3b43487aec4b419402841e2348c
Jake Goulding <jake.goulding@gmail.com>
2025-12-07 10:10:55 -0500

    Day 07 - Part 2

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,4 +1,7 @@
-use std::{collections::BTreeSet, mem};
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    mem,
+};
 
 const INPUT: &str = include_str!("../input.txt");
 
@@ -6,11 +9,37 @@ fn main() {
     let part1 = n_beam_splits(INPUT);
     assert_eq!(1594, part1);
     println!("{part1}");
+
+    let part2 = n_universes(INPUT);
+    assert_eq!(15650261281478, part2);
+    println!("{part2}");
 }
 
+#[derive(Default)]
+struct Manifold {
+    manifold: BTreeSet<(usize, usize)>,
+    h: usize,
+}
+
+type LaserPositions = BTreeMap<usize, usize>;
+
 fn n_beam_splits(s: &str) -> usize {
+    let (manifold, mut laser_positions) = parse_manifold(s);
+
+    run_experiment(&manifold, &mut laser_positions)
+}
+
+fn n_universes(s: &str) -> usize {
+    let (manifold, mut laser_positions) = parse_manifold(s);
+
+    run_experiment(&manifold, &mut laser_positions);
+
+    laser_positions.values().sum()
+}
+
+fn parse_manifold(s: &str) -> (Manifold, LaserPositions) {
     let mut manifold = BTreeSet::new();
-    let mut laser_positions = BTreeSet::new();
+    let mut laser_positions = BTreeMap::new();
     let mut h = 0;
 
     for (y, l) in s.lines().enumerate() {
@@ -23,7 +52,7 @@ fn n_beam_splits(s: &str) -> usize {
                 }
 
                 'S' => {
-                    laser_positions.insert(x);
+                    laser_positions.insert(x, 1);
                 }
 
                 o => panic!("Unknown character `{o}`"),
@@ -33,23 +62,27 @@ fn n_beam_splits(s: &str) -> usize {
         h = y;
     }
 
-    let mut next_laser_positions = BTreeSet::new();
+    (Manifold { manifold, h }, laser_positions)
+}
+
+fn run_experiment(manifold: &Manifold, laser_positions: &mut LaserPositions) -> usize {
+    let mut next_laser_positions = LaserPositions::new();
     let mut splits = 0;
 
-    for y in 0..h {
+    for y in 0..manifold.h {
         next_laser_positions.clear();
 
-        for &x in &laser_positions {
-            if manifold.contains(&(x, y + 1)) {
+        for (&x, &n) in &*laser_positions {
+            if manifold.manifold.contains(&(x, y + 1)) {
                 splits += 1;
-                next_laser_positions.insert(x - 1);
-                next_laser_positions.insert(x + 1);
+                *next_laser_positions.entry(x - 1).or_insert(0) += n;
+                *next_laser_positions.entry(x + 1).or_insert(0) += n;
             } else {
-                next_laser_positions.insert(x);
+                *next_laser_positions.entry(x).or_insert(0) += n;
             }
         }
 
-        mem::swap(&mut laser_positions, &mut next_laser_positions);
+        mem::swap(laser_positions, &mut next_laser_positions);
     }
 
     splits
@@ -64,5 +97,10 @@ mod test {
     #[test]
     fn part1_example() {
         assert_eq!(21, n_beam_splits(EXAMPLE));
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(40, n_universes(EXAMPLE));
     }
 }
